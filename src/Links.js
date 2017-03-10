551137edf3e8c67c6e20e60f6aff3ee2131792ab
VidyullathaKandipati <latha522@gmail.com>
2017-03-10 11:33:05 +1100

    Made the links to production

--- src/Links.js
+++ src/Links.js
@@ -8,10 +8,10 @@ let linkState = {
                               },
                   development: {
                                   isActive: false,
-                                  login: 'http://localhost:5000/login',
-                                  newUser: 'http://localhost:5000/users',
-                                  newinvite: 'http://localhost:5000/invites',
-                                  createfamily: 'http://localhost:5000/families'
+                                  login: 'https://singingframes.herokuapp.com/login',
+                                  newUser: 'https://singingframes.herokuapp.com/users',
+                                  newinvite: 'https://singingframes.herokuapp.com/invites',
+                                  createfamily: 'https://singingframes.herokuapp.com/families'
                                 }
                 };
 
